0fb3cc6613a1c2dcaba31e088fddf44863964bc8
Perry Baran <84458310+Squilly-flac@users.noreply.github.com>
2022-01-31 12:27:16 +0000

    Update gameLoop.js

--- src/gameLoop.js
+++ src/gameLoop.js
@@ -1,5 +1,7 @@
 const reset = require('./DOM/resetDOM');
-const temp = require('./DOM/setupDOM');
+const DOM = require('./DOM/gameLoopDOM');
+const AI = require('./AI');
+
 
 const gameLoop = (player1, player2, computer) => {
 
@@ -18,11 +20,44 @@ const gameLoop = (player1, player2, computer) => {
     gameboardP1.className = 'gameboard';
     containerBottom.appendChild(gameboardP1);
 
-    temp.gameboard(gameboardP2, player2);
-    temp.gameboard(gameboardP1, player1);
+    function loop(player, opponent, playerContainer, opponentContainer) {
+        reset(playerContainer);
+        reset(opponentContainer);
 
+        //display boards
+        DOM.board(player, playerContainer);                     //only need to display players board
+        const opponentBoard = DOM.board(opponent, opponentContainer);   //need to return values of opponents board for event listeners
 
+        //event listeners
+        if (player.getBoard().checkLose()) {    //check if player has lost
+            const winner = DOM.winner(player, opponent, playerContainer, opponentContainer);
+            if (opponent === player2) {
+                winner.win.className = 'winLose top'
+                winner.lose.className = 'winLose'
+            } else {
+                winner.win.className = 'winLose'
+                winner.lose.className = 'winLose top'
+            }
+            
+        } else if (computer && player === player2) {    //if player 2 is a computer and it's there turn
+            AI.play(opponent);
+            loop(opponent, player, opponentContainer, playerContainer);
+        } else { //players play
+            opponentBoard.forEach(cell => {
+                cell.addEventListener('click', () => {
+                    const board = opponent.getBoard()
+                    const y = cell.dataset.y;
+                    const x = cell.dataset.x;
 
+                    if (!board.checkBoard()[y][x].hit) { //if the board has not been hit
+                        board.receiveAttack(y, x);
+                        loop(opponent, player, opponentContainer, playerContainer); //reset loop with player/opponent swapped
+                    }
+                });
+            });
+        }
+    };
+    loop(player1, player2, gameboardP1, gameboardP2);
 }
 
 module.exports = gameLoop;
